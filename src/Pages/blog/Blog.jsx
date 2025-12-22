bbc5398637d94f286433ad41b32b2bcc0ec1e6f6
DnastyL(Lucas) <softwareengineerlucas@gmail.com>
2025-12-22 15:20:05 -0300

    Blog 60% concluido

---
+++ src/Pages/blog/Blog.jsx
@@ -0,0 +1,18 @@
+import "./blog.css";
+
+export default function Blog() {
+  const post = {
+    id: 48,
+    banner: "/reincidencia.jpg",
+    date: "2025-09-29T00:00",
+    slug: "reincidencia-inadimplencia-bate-recorde",
+    title:
+      "Reincidência na Inadimplência Bate Recorde: Como Proteger seu Crédito na Janela Crítica de 74 Dias",
+  };
+
+
+  
+  return <>{/* <Header/> */}
+
+  </>;
+}
